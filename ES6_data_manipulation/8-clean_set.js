741fcbd97b36a8d149a8ba553a7327f4690d49cc
Joaquinfer7688 <7688@holbertonstudents.com>
2024-05-11 16:26:11 -0700

    task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  const values = [];
+
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      values.push(value.substring(startString.length));
+    }
+  }
+  return values.join('-');
+}
